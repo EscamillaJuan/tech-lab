d180cd70a41f47b62c2be65a7ad11c4127ef2794
EscamillaJuan <juan.escamilla.dev@gmail.com>
2025-12-24 14:49:59 -0600

    add: concurrent examples

---
+++ concurrent-programming/src/main/java/examples/CountWordTask.java
@@ -0,0 +1,40 @@
+package examples;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CountWordTask extends RecursiveTask<Long> {
+    private final Path path;
+    private final String word;
+
+    public CountWordTask(Path path, String word) {
+        this.path = path;
+        this.word = word;
+    }
+
+    @Override
+    protected Long compute() {
+        if (!Files.isDirectory(path)) {
+            return WordCounter.countWords(path, word);
+        }
+        Stream<Path> subpaths;
+        try {
+            subpaths = Files.list(path);
+        } catch (IOException e) {
+            return 0L;
+        }
+        List<CountWordTask> subTasks =
+                subpaths.map(path -> new CountWordTask(path, word))
+                        .collect(Collectors.toList());
+        invokeAll(subTasks);
+        return subTasks
+                .stream()
+                .mapToLong(CountWordTask::getRawResult)
+                .sum();
+    }
+}
---
+++ concurrent-programming/src/main/java/examples/WordCounter.java
@@ -0,0 +1,55 @@
+package examples;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: WordCounter [path] [word]");
+            return;
+        }
+        Path start = Path.of(args[0]);
+        String word = args[1];
+
+        Instant before = Instant.now();
+
+        ForkJoinPool pool = new ForkJoinPool();
+
+        long count = pool.invoke(new CountWordTask(start, word));
+        //long count = countWords(start, word);
+
+        Duration elapsed = Duration.between(before, Instant.now());
+        System.out.println(count + " (" + elapsed.toMillis() + " milliseconds)");
+    }
+    public static long countWordsInFile(Path file, String word) {
+        try {
+            return Files.readAllLines(file, StandardCharsets.UTF_8)
+                    .stream()
+                    .flatMap(l -> Arrays.stream(l.split(" ")))
+                    .filter(word::equalsIgnoreCase)
+                    .count();
+        } catch (IOException e) {
+            return 0;
+        }
+    }
+
+    public static long countWords(Path path, String word) {
+        if (!Files.isDirectory(path)) {
+            return countWordsInFile(path, word);
+        }
+        try {
+            return Files.list(path)
+                    .mapToLong(p -> countWords(p, word))
+                    .sum();
+        } catch (IOException e) {
+            return 0;
+        }
+    }
+}
